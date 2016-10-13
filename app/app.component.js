81b35ae43b6b1ae939763503653b82a3183b9eed
code-for-coffee <jtraverjr@gmail.com>
2016-10-13 15:01:07 -0500

    basic component fleshed out

--- app/app.component.js
+++ app/app.component.js
@@ -1,9 +1,10 @@
 (function(app) {
     app.AppComponent =
         ng.core.Component({
-
+            selector: 'playground-app',
+            template: '<h3>Playground</h3><p>Fun!</p>'
         })
         .Class({
-
+            constructor: function() {}
         });
 })(window.app || (window.app = {}));
